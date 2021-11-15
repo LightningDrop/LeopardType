1db972294ef7e1a3ed77e9c9e39c38f0a18b6f8a
nhidalgo20 <61564695+nhidalgo20@users.noreply.github.com>
2021-11-15 00:34:41 -0800

    Added passwordDBContext, but Enable-Migrations not working

---
+++ LeopardType.Core/Model/Passwords.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeopardType.Core.Models
+{
+    public class Passwords
+    {
+        [Key]
+        public int PasswordId { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string? Password { get; set; }
+    }
+}
---
+++ LeopardType.EF/PasswordDBContext.cs
@@ -0,0 +1,13 @@
+﻿using LeopardType.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeopardType.EF
+{
+    class PasswordDBContext : DbContext
+    {
+
+        public DbSet<Passwords>? Password { get; set; }
+
+
+    }
+}
